0dadcd9025c883bc66bc93a770c1c47a4ac59fa7
Kolade Amire <stephamire@gmail.com>
2025-12-18 15:13:21 +0100

    chore: bootstrap pfie backend

---
+++ src/main/java/com/kay/pfie/PfieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kay.pfie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kay/pfie/PfieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kay.pfie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
